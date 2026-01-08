87a43bdfd4c857db2e557a68173e60401b378985
Aditya Modi <modiaditya642@gmail.com>
2026-01-08 13:25:18 +0530

    Modified test with more examples

--- test.py
+++ test.py
@@ -2,21 +2,23 @@ import phimath as pm
 
 E = pm.E
 PI = pm.PI
-a=pm.vector(3,60,30, mode="polar")
-b=pm.vector(1,2,0, mode=None)
-c= pm.matrix([[1,2,3],[4,5,6]])
-d= pm.matrix([[7,8,9],[10,11,12]])
-print(PI, E)
-print(pm.sin(PI/2.0))
-print(pm.exp(0))          # 1.0
-print(pm.exp(1))          # ≈ 2.71828
-print(pm.ln(pm.E))   # ≈ 1.0
-print(pm.log(100, 10))    # ≈ 2.0
-print(pm.exp(pm.ln(5)))  # ≈ 5.0
-print(b * 6)        
-print (c + d)
-print (PI+2)
-a = [1,1,7]
-b=[2,-1,2]
-print(pm.solve_linear_system(a,b))
-print(pm.quadratic_solver(1,-3,2))
+a=pm.vector(3,60,30, mode="polar")                      # vector a in polar coordinates
+b=pm.vector(1,2,0, mode=None)                           # vector b in Cartesian coordinates
+c= pm.matrix([[1,2,3],[4,5,6]])                         # 2x3 Matrix c
+d= pm.matrix([[7,8,9],[10,11,12]])                      # 2x3 Matrix b
+print(PI, E)                                            # Print constants
+print(pm.sin(PI/2.0))                                   # ≈ 1.0
+print(pm.exp(0))                                        # ≈ 1.0
+print(pm.exp(1))                                        # ≈ 2.71828
+print(pm.ln(pm.E))                                      # ≈ 1.0
+print(pm.log(100, 10))                                  # ≈ 2.0
+print(pm.exp(pm.ln(5)))                                 # ≈ 5.0
+print(b * 6)                                            # Scalar multiplication
+print (c + d)                                           # Matrix addition
+print (a + b)                                           # Vector addition
+print (a.dot(b))                                        # Dot product
+a = [1,1,7]                                             # Coefficients of equations 1
+b=[2,-1,2]                                              # Coefficients of equations 2
+print(pm.solve_linear_system(a,b))                      # Solve linear equations    
+print(pm.quadratic_solver(1,-3,2))                      # Solve quadratic equations
+print(pm.quadratic_solver(1,2,5))                       # Solve quadratic equations with complex roots
